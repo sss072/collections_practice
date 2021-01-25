120a910cfae603a7fef00ed16cac6bd89b50a808
Sami <samsabeeh6@gmail.com>
2021-01-24 21:07:16 -0500

    commit tree a440163f2ab41d4b6dc1d54a5749693931645226

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,76 @@
+def sort_array_asc(array)
+    array.sort do |a,b|
+        if a == b 
+            0
+        elsif a < b
+            -1 
 
+        elsif a > b 
+            1 
+        end 
+    end 
+end 
+
+def sort_array_desc(array)
+    array.sort do |a,b|
+        if a == b 
+            0 
+        elsif a > b 
+            -1 
+        elsif a < b 
+            1 
+        end 
+    end 
+end 
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        if a.length == b.length 
+            0 
+        elsif a.length < b.length
+            -1 
+        elsif a.length > b.length 
+            1 
+        end 
+    end 
+end 
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse 
+end 
+
+def kesha_maker(array)
+    array.each do |item|
+      item[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.find_all do |word|
+      word[0] == "a"
+
+    end
+end 
+
+def sum_array(array)
+    sum = 0
+    array.each do |num|
+      sum+=num
+    end
+    sum
+end 
+
+def add_s(array)
+    array.collect do |word|
+      if array[1] == word
+        word
+      else
+        word + "s"
+      end
+    end
+end
